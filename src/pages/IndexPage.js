eb06eb9b8d5c62580b1ec99bc427db33f7627a9b
aizhw <aizhw@yonyou.com>
2021-06-29 22:52:50 +0800

    路由优化2

--- src/pages/IndexPage.js
+++ src/pages/IndexPage.js
@@ -2,22 +2,26 @@ import { connect } from 'dva';
 import styles from './IndexPage.scss'
 
 // 引入路由需要的组件 azw
-import { Switch, Route , Redirect  } from 'dva/router';
- 
+import { Switch } from 'dva/router';
+import SubRoutes, { RedirectRoute, NoMatchRoute } from '../utils/SubRoutes'
 
 import NavBar from './NavBar'
-import Home from './Home'
-import About from './About'
-import Admin from './Admin'
-import Menus from './Menus'
-import Login from './User/Login'
-import Register from './User/Register'
+// import Home from './Home'
+// import About from './About'
+// import Admin from './Admin'
+// import Menus from './Menus'
+// import Login from './User/Login'
+// import Register from './User/Register'
 import { Layout} from 'antd';
+
 
 
 const { Header, Content } = Layout;
 
+
 function IndexPage(props) {
+
+  const { routes } = props;
   return <Layout className={styles.layout}>
     <Header className={styles.header}>
       <NavBar {...props}/>
@@ -25,14 +29,30 @@ function IndexPage(props) {
     <Content className={styles.content}>
       {/* 一级路由 */}
       <Switch>
-        <Route path='/home' component = {Home} />
+        {/* <Route path='/home' component = {Home} />
         <Route path='/admin' component = {Admin} />
         <Route path='/about' component = {About} />
         <Route path='/menus' component = {Menus} />
         <Route path='/login' component = {Login} />
-        <Route path='/register' component = {Register} />
+        <Route path='/register' component = {Register} /> */}
+
+         {routes .map((router, i) => (
+          // 调用路由组件
+          <SubRoutes key={i}  {...router} />
+        ))} 
+
+        {/*  
+          重定向方式：
+          如果路由配置中没有redirect: true（通过循环渲染重定向）
+          则默认第一个路由为重定向路由
+          <Redirect exact from={"/"} to={routes[0].path} /> 
+        */}
         {/* 重定向 */}
-        <Redirect to="/home" />
+        {/* <Redirect to="/home" /> */}
+        <RedirectRoute exact={true} from={'/'} routes={routes} />
+
+        {/* 输入的链接不存在时,跳转到NoMatch组件中 */}
+        <NoMatchRoute />
       </Switch>
     </Content>
   </Layout>
--- src/utils/SubRoutes.js
+++ src/utils/SubRoutes.js
@@ -1,20 +1,39 @@
 import React from 'react'
 import { Route } from 'dva/router' 
+import { Redirect } from 'dva/router';
+import NoMatch from '../components/NoMatch';
 
-export default function subRoutes(route) {
-  console.log(route)
+// export default function subRoutes(route) {
+//   console.log(route)
+//   return (
+//     <Route 
+//       render={props => <route.component {...props} routes={route.routes} />} 
+//     />
+//   )
+// }
+
+
+export default function subRoutes({routes, component: Component}) {
   return (
     <Route 
-      render={props => <route.component {...props} routes={route.routes} />} 
+        render={props => <Component {...props} routes={routes} />} 
     />
   )
 }
 
 
-// export default function subRoutes({routes, component: Component}) {
-//   return (
-//     <Route 
-//         render={props => <Component {...props} routes={routes} />} 
-//     />
-//   )
-// }
+// RedirectRoute 封装重定向
+export function RedirectRoute({routes, from, exact}) {
+  const routeR = routes.filter(item => {
+    return item.direction
+  })
+
+  const to = routeR.length ? routeR[0].path : routes[0].path;
+  return <Redirect exact={exact} from={from} to={to} />;
+}
+
+
+// NoMatchRoute
+export function NoMatchRoute({ status = 404 }) {
+  return <Route render={props => <NoMatch {...props} status={status} />} />;
+}
